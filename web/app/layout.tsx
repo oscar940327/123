4265c6f25f8172f8d47424b3e68acb91d70deb19
oscar940327 <oscar940327@gmail.com>
2026-07-19 15:07:29 +0800

    style(web): switch landing page to warm minimal palette

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Noto_Sans_TC } from "next/font/google";
 import "./globals.css";
+import "./theme.css";
 
 const notoSans = Noto_Sans_TC({
   variable: "--font-noto",
